c5f9bf1df6eab33c85f7c2061d3a4b797737c74e
Samuel <duartes607@gmail.com>
2024-07-27 21:03:22 -0300

    add: created token expiration validation;

--- src/main/java/com/example/AuthService/infra/security/SecurityFilter.java
+++ src/main/java/com/example/AuthService/infra/security/SecurityFilter.java
@@ -1,5 +1,6 @@
 package com.example.AuthService.infra.security;
 
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.example.AuthService.repositories.UserRepository;
 import com.example.AuthService.services.TokenService;
 import jakarta.servlet.FilterChain;
@@ -26,16 +27,20 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
         var token = this.recoverToken(request);
         if(token != null){
            var login = this.tokenService.validateToken(token);
             UserDetails user = userRepository.findByLogin(login);
-
             var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
 
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
         filterChain.doFilter(request, response);
+        } catch (JWTVerificationException verificationException) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().println(verificationException.getMessage());
+        }
     }
 
     private String recoverToken(HttpServletRequest request){
